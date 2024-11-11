60e186c4be12cc00156b06fee5c8f9ecae24378f
alemiranda <alejandroxq52@gmail.com>
2024-11-11 04:11:26 -0300

    agregue function , while y switch.

--- main.js
+++ main.js
@@ -1,33 +1,70 @@
+const menu = "1- Calcular consumo, 2- Salir";
+const opcionElegida = parseInt(prompt("Ingrese la opción: " + menu));
+const valor = 9635
 
-         const litros = parseFloat =(prompt("ingrese cantidad de litros"));
-         const valor = 9635
-          if (litros >= 0  && litros <= 25 ){
-                alert (" su deuda por el mes de octubre es de  $9635")
-            }
-        else if(litros >= 26  && litros <= 30 ){
-                const excedente = (litros - 25);
-                const precioTotal = excedente * 385;
-                alert("su deuda es de  $" + ( precioTotal + valor) );
-            }
-            
-        else if(litros >= 31  && litros <= 50 ){
-                const excedente = (litros - 25);
-                const precioTotal = excedente * 770;
-                alert("su deuda es de $ " + ( precioTotal + valor) );
-                
-            }
+function consultaDeuda(opcionElegida){
+     let deuda = parseFloat(prompt("ingrese cantidad de litros"))
+     
+     let precioTotal;
+     
+     while(isNaN(deuda)){
+        alert("datos invalidos");
+        deuda = parseInt(prompt("ingrese cantidad de litros"));
+        break
+        }
+     if (deuda >= 0  && deuda <= 25 ){
+       return valor;
+    }
+else if(deuda >= 26  && deuda <= 30 ){
+        const excedente = (deuda - 25);
+        const valorExcedente = excedente * 385;
+        let precioTotal = valor + valorExcedente;
+        return precioTotal;
+    }
+else if(deuda >= 31  && deuda <= 50 ){
+        const excedente = (deuda - 25);
+        const valorExcedente = excedente * 770;
+        let precioTotal = valor + valorExcedente;
+        return precioTotal;
+    }
+else if(deuda >= 51  && deuda < 81 ){
+        const excedente = (deuda - 25);
+        const valorExcedente = excedente * 1926;
+        let precioTotal = valor + valorExcedente;
+       return precioTotal;
+    }
+else if(deuda >= 81  && deuda < 500 ){
+        const excedente = (deuda - 25);
+        const valorExcedente = excedente * 2697;
+        let precioTotal = valor + valorExcedente;
+       return precioTotal;
+    }
+    return precioTotal ;
+}
 
-            
-          else if(litros >= 51  && litros < 80 ){
-                const excedente = (litros - 25);
-                const precioTotal = excedente * 1926;
-                alert("su deuda es de  $" + ( precioTotal + valor) );
-                
-            }
-            
-           else if(litros >= 81  && litros < 500 ){
-                const excedente = (litros - 25);
-                const precioTotal = excedente * 2697;
-                alert("su deuda es de $" + ( precioTotal + valor) );
-            }
+while(isNaN(opcionElegida)){
+    alert("Ingrese un número válido");
+    opcionElegida = parseInt(prompt("Ingrese la opción: " + menu));
+    }
 
+switch(opcionElegida){
+    case 1 :
+        let deuda = consultaDeuda(); 
+        alert("Su consumo es de $" + deuda); 
+        break;
+         default: alert("Opción no válida.");
+         break
+
+    case 2 :   
+        if (opcionElegida == 2){
+            alert ("gracias por visitarnos")
+        }
+ 
+        }
+ 
+
+    
+
+
+
+ 
